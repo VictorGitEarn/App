66effc69383193ef7c1f95d45af779dc46b7f3c2
Victor Alves <victor.earn@outlook.com>
2021-02-28 22:50:38 -0300

    Angular Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'App';
+
+  orders: any;
+
+  constructor(private http: HttpClient) { }
+  // tslint:disable-next-line:no-trailing-whitespace
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+  // tslint:disable-next-line:typedef
+  onSumit(data){
+    this.http.post('https://localhost:44302/Order', data).subscribe();
+    window.location.reload();
+  }
+
+  // tslint:disable-next-line:typedef
+  getOrders() {
+    this.http.get('https://localhost:44302/Order').subscribe(response => { this.orders = response; }, error => { console.log(error); });
+  }
 }
